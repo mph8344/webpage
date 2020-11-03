e4c4c9dfe30a943edca30f7d028ccd6ebdb3c0db
mph8344 <mph8344@g.rit.edu>
2020-11-02 21:57:39 -0500

    Made style changes and added a sticky nav bar

--- dynamic.js
+++ dynamic.js
@@ -1,65 +1,18 @@
 
-window.onload = function() {
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
-    var color1 = document.getElementById("color1");
-    var color2 = document.getElementById("color2");
-    var color3 = document.getElementById("color3");
-    var color4 = document.getElementById("color4");
-    var color5 = document.getElementById("color5");
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-    color1.onclick = changeColor
-    color2.onclick = changeColor
-    color3.onclick = changeColor
-    color4.onclick = changeColor
-    color5.onclick = changeColor
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
 
-
-}
-
-
-function editBox() {
-
-    var thisButton = document.getElementById("textbutton");
-    var value = thisButton.getAttribute("value");
-    if (value==="more") {
-        thisButton.innerText = "Less";
-        thisButton.value = "less";
-        createBox();
-    } else {
-        thisButton.innerText = "More...";
-        thisButton.value = "more";
-        deleteBox();
-    }
-}
-function createBox() {
-
-    var element = document.getElementById("hiddentext");
-    /*element.innerText="Ah, distinctly I remember it was in the bleak December" +
-    " And each separate dying ember wrought its ghost upon the floor." +
-    " Eagerly I wished the morrow;—vainly I had sought to borrow" + 
-    " From my books surcease of sorrow—sorrow for the lost Lenore —" +
-    " For the rare and radiant maiden whom the angels name Lenore —" +
-    " Nameless here for evermore.";*/
-    element.style.display= "block"
-    
-
-}
-
-function deleteBox() {
-    var element = document.getElementById("hiddentext");
-    //element.innerText=""
-    element.style.display= "none"
-}
-
-function changeFont(font) {
-    console.log(font.value);
-    document.getElementById('textbox').style.fontFamily = font.value;
-    document.getElementById('hiddentext').style.fontFamily = font.value;
-    
-}
-
-function changeColor() {
-    console.log(this.value);
-    document.getElementById('textbox').style.color = this.value;
-    document.getElementById('hiddentext').style.color = this.value;
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
